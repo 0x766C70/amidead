75ca22c9565c816456098c0d45c902ce6baedbb6
vlp <thomas@criscione.fr>
2020-05-24 22:38:02 +0200

    1st

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>amidead</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="vlp" content="amidead_project">
+  <link rel="stylesheet" href="https://unpkg.com/chota@latest">
+</head>
+<body>
+hello world
+</body>
+</html>
