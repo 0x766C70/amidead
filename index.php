716e234e1eab20aee0955f1f4d2a717b5fd59425
vlp <thomas@criscione.fr>
2020-05-24 23:21:30 +0200

    index

--- index.php
+++ index.php
@@ -7,12 +7,8 @@
   <title>amidead</title>
   <meta name="description" content="The HTML5 Herald">
   <meta name="vlp" content="amidead_project">
-  <link rel="stylesheet" href="https://unpkg.com/chota@latest">
 </head>
 <body>
-<div class="row">
-  <div class="col"> .col </div>
-  <div class="col"> .col </div>
-</div
+I'm not dead !
 </body>
 </html>
