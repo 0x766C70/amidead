314e8d98e298c73abfcc1b700ccb66f11fdeffcc
vlp <thomas@criscione.fr>
2020-05-25 16:59:02 +0200

    r4

--- check.sh
+++ check.sh
@@ -26,7 +26,7 @@ if [ "$lastPing" == "koSOS" ] && [ "$diffPing" -ge $timeSOS ]; then
 	echo -e $now >> log
 	echo -e "SOS" >> log
 elif [ "$lastPing" == "ko" ] && [ "$diffPing" -ge $timeLastCall ]; then
-	echo -e "Subject: Are you alive ? \nGo to $url to say that you are alive ! THIS IS THE LAST CALL" | msmtp "$myMail"
+	echo -e "Subject: LAST CALL: Are you alive ? \nGo to $url to say that you are alive ! THIS IS THE LAST CALL" | msmtp "$myMail"
         echo -e $now >> log
 	echo -e "koSOS" >> log
 elif [ "$lastPing" != "SOS" ] && [ "$diffPing" -ge $timeMail ]; then
