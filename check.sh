615f7e6f25c3f493217638e61e8d164749d10ad9
vlp <thomas@criscione.fr>
2020-05-25 16:09:50 +0200

    final

--- check.sh
+++ check.sh
@@ -15,25 +15,24 @@ now=$( date +'%Y-%m-%dT%H:%M:%S' )
 lastPing=$( tail -n 1 log )
 previousPing=$( tail -n 2 log | head -n 1 )
 
-if [ $lastPing == "ko" ] || [ $lastPing == "koSOS" ]; then
+if [ $lastPing == "ko" ] || [ $lastPing == "koSOS" ] || [ $lastPing == "SOS" ]; then
 	diffPing=$( dateutils.ddiff $previousPing $now -f %$units )
 else
 	diffPing=$( dateutils.ddiff $lastPing $now -f %$units )
 fi
 
-echo $lastPing
-echo $diffPing
-
-if [ "$lastPing" == "koSOS" ] && [ "diffPing" > $timeSOS ]; then
-	echo "Subject:SOS MAIL of $myMail \n$(cat ./message)" | msmtp "$recipient"
+if [ "$lastPing" == "koSOS" ] && [ "$diffPing" -ge $timeSOS ]; then
+	echo -e "Subject:SOS MAIL of $myMail \n$(cat ./message)" | msmtp "$recipient"
 	echo -e $now >> log
 	echo -e "SOS" >> log
-elif [ "$lastPing" == "kolc" ] && [ "diffPing" > $timeLastCall ]; then
-	echo "Subject: Are you alive \nGo to $url to say that you are alive ! THIS IS THE LAST CALL" | msmtp $myMail
+elif [ "$lastPing" == "ko" ] && [ "$diffPing" -ge $timeLastCall ]; then
+	echo -e "Subject: Are you alive \nGo to $url to say that you are alive ! THIS IS THE LAST CALL" | msmtp "$myMail"
         echo -e $now >> log
 	echo -e "koSOS" >> log
-elif [ "diffPing" > $timeMail ]; then
-        echo "Subject: Are you alive \nGo to $url to say that you are alive !" | msmtp $myMail
+elif [ "$diffPing" -ge $timeMail ]; then
+        echo -e "Subject: Are you alive \nGo to $url to say that you are alive !" | msmtp "$myMail"
         echo -e $now >> log
         echo -e "ko" >> log
+elif [ "$lastPing" == "SOS" ]; then
+	echo "X_x"
 fi
